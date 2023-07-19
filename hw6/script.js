b6f29ea6d82ad863992aaf187318c6c7e3f2cbc3
Tünzalə <114104944+Tunzale1@users.noreply.github.com>
2023-07-19 21:59:51 +0400

    fixed!

--- hw6/script.js
+++ hw6/script.js
@@ -1,9 +1,10 @@
 async function IP() {
+  const apiKey = "b22b7525f748431641086486d300a36d";
   try {
-   const Request = await fetch("https://api.ipify.org/?format=json");
-    const { ip } = await Request.json();
-    const locationRequest = await fetch(`http://ip-api.com/json/${ip}`);
-    const location = await locationRequest.json();
+    const ipResponse = await fetch("https://api.ipify.org/?format=json");
+    const { ip } = await ipResponse.json();
+    const locationResponse = await fetch(`http://api.ipstack.com/${ip}?access_key=${apiKey}`);
+    const location = await locationResponse.json();
     display(location);
   } catch (error) {
     console.error(error);
@@ -13,10 +14,12 @@ async function IP() {
 function display(location) {
   const container = document.getElementById("container");
   container.innerHTML = `
-  <p>Continent - ${location.continent}<p>
-  <p>Country - ${location.country}</p>
-  <p>Region - ${location.regionName}</p>
-  <p>City - ${location.city}</p>
-  <p>District - ${location.district}</p>
+    <p>Continent - ${location.continent_name}<p>
+    <p>Country - ${location.country_name}</p>
+    <p>Region - ${location.region_name}</p>
+    <p>City - ${location.city}</p>
+    <p>District - ${location.district || "Not available"}</p>
   `;
 }
+
+IP();
