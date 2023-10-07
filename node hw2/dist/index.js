15dd0102daf8c77c2010d43acf3ef5099f0f88b0
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2023-10-07 12:46:41 +0400

    likehood fixed

--- node hw2/dist/index.js
+++ node hw2/dist/index.js
@@ -1,49 +1,61 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Transaction = exports.CurrencyEnum = exports.Card = void 0;
-const uuid_1 = require("uuid");
+var uuid_1 = require("uuid");
 var CurrencyEnum;
 (function (CurrencyEnum) {
     CurrencyEnum[CurrencyEnum["USD"] = 0] = "USD";
     CurrencyEnum[CurrencyEnum["UAH"] = 1] = "UAH";
 })(CurrencyEnum || (exports.CurrencyEnum = CurrencyEnum = {}));
-class Transaction {
-    constructor(amount, currency) {
+var Transaction = /** @class */ (function () {
+    function Transaction(amount, currency) {
         this.Id = (0, uuid_1.v4)();
         this.Amount = amount;
         this.Currency = currency;
     }
-}
+    return Transaction;
+}());
 exports.Transaction = Transaction;
-class Card {
-    constructor() {
+var Card = /** @class */ (function () {
+    function Card() {
         this.transactions = [];
     }
-    AddTransaction(transaction) {
+    Card.prototype.AddTransaction = function (transaction) {
         this.transactions.push(transaction);
         return transaction.Id;
-    }
-    AddTransactionWith(amount, currency) {
-        const transaction = new Transaction(amount, currency);
-        this.transactions.push(transaction);
-        return transaction.Id;
-    }
-    GetTransaction(transactionId) {
-        for (const transaction of this.transactions) {
+    };
+    Card.prototype.AddTransactionWith = function (arg1, arg2) {
+        if (typeof arg1 === 'number' && arg2 !== undefined) {
+            var transaction = new Transaction(arg1, arg2);
+            this.transactions.push(transaction);
+            return transaction.Id;
+        }
+        else if (arg1 instanceof Transaction) {
+            this.transactions.push(arg1);
+            return arg1.Id;
+        }
+        else {
+            throw new Error("Invalid arguments for AddTransactionWith");
+        }
+    };
+    Card.prototype.GetTransaction = function (transactionId) {
+        for (var _i = 0, _a = this.transactions; _i < _a.length; _i++) {
+            var transaction = _a[_i];
             if (transaction.Id === transactionId) {
                 return transaction;
             }
         }
         return undefined;
-    }
-    GetBalance(currency) {
-        const totalAmount = this.transactions.reduce((sum, transaction) => {
+    };
+    Card.prototype.GetBalance = function (currency) {
+        var totalAmount = this.transactions.reduce(function (sum, transaction) {
             if (transaction.Currency === currency) {
                 sum += transaction.Amount;
             }
             return sum;
         }, 0);
         return totalAmount;
-    }
-}
+    };
+    return Card;
+}());
 exports.Card = Card;
--- node hw2/src/index.ts
+++ node hw2/src/index.ts
@@ -1,53 +1,64 @@
 import { v4 as uuidv4 } from "uuid";
 
 enum CurrencyEnum {
-    USD,
-    UAH,
+  USD,
+  UAH,
+}
+
+class Transaction {
+  readonly Id: string;
+  readonly Amount: number;
+  readonly Currency: CurrencyEnum;
+
+  constructor(amount: number, currency: CurrencyEnum) {
+    this.Id = uuidv4();
+    this.Amount = amount;
+    this.Currency = currency;
+  }
+}
+
+class Card {
+  private transactions: Transaction[] = [];
+
+  AddTransaction(transaction: Transaction): string {
+    this.transactions.push(transaction);
+    return transaction.Id;
   }
 
-  class Transaction {
-    readonly Id: string;
-    readonly Amount: number;
-    readonly Currency: CurrencyEnum;
+  AddTransactionWith(amount: number, currency: CurrencyEnum): string;
+  AddTransactionWith(transaction: Transaction): string;
   
-    constructor(amount: number, currency: CurrencyEnum) {
-      this.Id = uuidv4();
-      this.Amount = amount;
-      this.Currency = currency;
+  AddTransactionWith(arg1: number | Transaction, arg2?: CurrencyEnum): string {
+    if (typeof arg1 === 'number' && arg2 !== undefined) {
+      const transaction = new Transaction(arg1, arg2);
+      this.transactions.push(transaction);
+      return transaction.Id;
+    } else if (arg1 instanceof Transaction) {
+      this.transactions.push(arg1);
+      return arg1.Id;
+    } else {
+      throw new Error("Invalid arguments for AddTransactionWith");
     }
   }
 
-  class Card {
-    private transactions: Transaction[] = [];
-  
-    AddTransaction(transaction: Transaction): string {
-      this.transactions.push(transaction);
-      return transaction.Id;
-    }
-  
-    AddTransactionWith(amount: number, currency: CurrencyEnum): string {
-      const transaction = new Transaction(amount, currency);
-      this.transactions.push(transaction);
-      return transaction.Id;
-    }
-  
-    GetTransaction(transactionId: string): Transaction | undefined {
-        for (const transaction of this.transactions) {
-          if (transaction.Id === transactionId) {
-            return transaction;
-          }
-        }
-        return undefined;
+  GetTransaction(transactionId: string): Transaction | undefined {
+    for (const transaction of this.transactions) {
+      if (transaction.Id === transactionId) {
+        return transaction;
       }
-  
-    GetBalance(currency: CurrencyEnum): number {
-      const totalAmount = this.transactions.reduce((sum, transaction) => {
-        if (transaction.Currency === currency) {
-          sum += transaction.Amount;
-        }
-        return sum;
-      }, 0);
-      return totalAmount;
     }
+    return undefined;
   }
-  export { Card, CurrencyEnum,Transaction };
+
+  GetBalance(currency: CurrencyEnum): number {
+    const totalAmount = this.transactions.reduce((sum, transaction) => {
+      if (transaction.Currency === currency) {
+        sum += transaction.Amount;
+      }
+      return sum;
+    }, 0);
+    return totalAmount;
+  }
+}
+
+export { Card, CurrencyEnum, Transaction };
